f793f18094b5e90c539ab23559390881c2e1d57a
DausDede <dauslearn@gmail.com>
2025-07-15 23:36:57 +0700

    test: add basic widget test

---
+++ test/widget_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_todo_app/main.dart';
+
+void main() {
+  testWidgets('MyApp builds without crashing', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+}
